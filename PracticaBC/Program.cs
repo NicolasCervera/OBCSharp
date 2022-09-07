2a34f13c676c614020be0c450db9d36400103d3b
Cervera Emilio Nicolas <ecervera@ext.prevencionart.com.ar>
2022-09-07 11:18:18 -0300

    Deploy

---
+++ PracticaBC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticaBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es: " + nombre);
+
+            Console.WriteLine("Ingrese una hora: ");
+            string hora = Console.ReadLine();
+            Console.WriteLine("La hora es: " + hora);
+        }
+    }
+}
